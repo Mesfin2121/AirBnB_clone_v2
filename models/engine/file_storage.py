948ca0b69e8e4a82b96835ef94a388934be5d39c
Mesfin2121 <medmesfin20@gmail.com>
2023-03-23 14:06:29 -0700

    fix Task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+
+classes = {"BaseModel": BaseModel, "User": User, "State": State, "City": City,
+           "Amenity": Amenity, "Place": Place, "Review": Review}
 
 
 class FileStorage:
@@ -8,9 +19,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if not cls:
+            return FileStorage.__objects
+        if cls in classes.values():
+            cp = {}
+            for k, v in FileStorage.__objects.items():
+                if isinstance(v, cls):
+                    cp[k] = FileStorage.__objects[k]
+            return cp
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
